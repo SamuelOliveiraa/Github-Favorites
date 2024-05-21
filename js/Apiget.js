4ceac63a9d8eff0fb8079e444d9933d779db0ecf
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2024-05-21 11:20:27 -0300

    Update Apiget.js

--- js/Apiget.js
+++ js/Apiget.js
@@ -1,13 +1,14 @@
+async function getUserFromApi(userName) {
+  const endpoint = `https://api.github.com/users/${userName}`;
 
+  const userData = await fetch(endpoint)
+    .then(data => data.json())
+    .then(data => data);
 
+  /*  if (userData.message) {
+    showErrorMessage("Usuario não encotrado, tente novamente");
+    return;
+  }
 
-
-
-
-
-
-
-
-
-
-
+  createUser(userData); */
+}
